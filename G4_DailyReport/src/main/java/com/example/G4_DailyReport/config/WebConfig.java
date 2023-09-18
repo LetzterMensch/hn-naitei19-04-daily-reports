e0ce8d0664eb685d886daef482aebc685fe9c0b8
LetzterMensch <letrongnghiavanyen@gmail.com>
2023-09-18 15:58:18 +0700

    modified WebConfig

--- G4_DailyReport/src/main/java/com/example/G4_DailyReport/config/WebConfig.java
+++ G4_DailyReport/src/main/java/com/example/G4_DailyReport/config/WebConfig.java
@@ -1,10 +1,7 @@
 package com.example.G4_DailyReport.config;
 
-<<<<<<< HEAD
-=======
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.context.annotation.Bean;
->>>>>>> c91ea22949c0995572eaeeeee2065396af86422e
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,11 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/reports/new").setViewName("user/new_report");
     }
-<<<<<<< HEAD
-=======
     @Bean
     public LayoutDialect layoutDialect() {
         return new LayoutDialect();
     }
->>>>>>> c91ea22949c0995572eaeeeee2065396af86422e
 }
